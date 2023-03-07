4321a67daf86d2c2f2e06ef77a5c393a6d695e7c
Alexander <nicky-nico-1998@outlook.com>
2023-03-07 23:40:51 +0000

    style added to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,36 @@ function Home() {
       <p>
         Front End Web Developer
       </p>
-      <p></p>
+      <p className="list-text">I like to code and learn new stuff</p>
+      <div className="container-lists">
+        <h2 className="head-list-1">Some of my skills are:</h2>
+        <ul className="list-one">
+          <li>
+            HTML
+          </li>
+          <li>
+            CSS
+          </li>
+          <li>
+            JavaScript
+          </li>
+          <li>
+            jQuery
+          </li>
+          <li>
+            Bootstrap
+          </li>
+        </ul>
+      </div>
+      <div className="container-lists">
+        <h2 className="head-list-2">I'm currently learning and improving:</h2>
+        <ul className="list-two">
+          <li>React</li>
+          <li>JavaScript</li>
+          <li>Ajax</li>
+          <li>Node.js</li>
+        </ul>
+      </div>
     </div>
   );
 }
