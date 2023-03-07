3e54303c68bea2ccd335ac3f572df7c90208dade
Alexander <nicky-nico-1998@outlook.com>
2023-03-07 00:31:06 +0000

    title of projects style changed

--- my-portfolio/src/components/ContactPage/Contact.js
+++ my-portfolio/src/components/ContactPage/Contact.js
@@ -7,14 +7,16 @@ function Contact(git) {
   return (
     <div>
       <h1>Contact Page</h1>
-      <p className='contact-p'>
-        Get in Touch!
-      </p>
-      <div className='contact-information'>
-        <a href='mailto:nicky-nico-1998@outlook.com'>Email</a>
-        <br></br>
-        <a href={cv} download="CV">Download CV</a>
-      </div >
+      <container className="contact-container">
+        <p className='contact-p'>
+          Get in Touch!
+        </p>
+        <div className='contact-information'>
+          <a href='mailto:nicky-nico-1998@outlook.com'>Email</a>
+          <br></br>
+          <a href={cv} download="CV">Download CV</a>
+        </div >
+      </container>
     </div>
   );
 }
