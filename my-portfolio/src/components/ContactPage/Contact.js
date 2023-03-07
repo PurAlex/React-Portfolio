5098cf7082e4cdb2be34094450823e9678346a27
Alexander <nicky-nico-1998@outlook.com>
2023-03-07 15:17:24 +0000

    contact form added to contact page

--- my-portfolio/src/components/ContactPage/Contact.js
+++ my-portfolio/src/components/ContactPage/Contact.js
@@ -3,7 +3,7 @@ import { Link, Route, Routes } from 'react-router-dom';
 import "./contact.css";
 import cv from '../../assets/CV(Nicky).pdf';
 
-function Contact(git) {
+function Contact() {
   return (
     <div>
       <h1>Contact Page</h1>
@@ -11,6 +11,62 @@ function Contact(git) {
         <p className='contact-p'>
           Get in Touch!
         </p>
+        <form
+          action="https://getform.io/f/04feb2e1-24e2-4e73-98b3-837225c11041"
+          method="POST"
+        >
+          <div className="row ">
+            <div className="col">
+              <input
+                type="text"
+                className="form-control"
+                name="first_name"
+                placeholder="First name"
+              />
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                className="form-control"
+                name="last_name"
+                placeholder="Last name"
+              />
+            </div>
+          </div>
+          <div className="row my-3">
+            <div className="col">
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                className="form-control"
+                name="subject"
+                placeholder="Subject"
+              />
+            </div>
+          </div>
+          <div className="row my-3">
+            <div className="col">
+              <textarea
+                className="form-control"
+                name="message"
+                placeholder="Message"
+                rows="6"
+              ></textarea>
+            </div>
+          </div>
+          <div className="row">
+            <button type="submit" className="btn btn-custom btn-primary">
+              Send
+            </button>
+          </div>
+        </form>
         <div className='contact-information'>
           <a href='mailto:nicky-nico-1998@outlook.com'>Email</a>
           <br></br>
